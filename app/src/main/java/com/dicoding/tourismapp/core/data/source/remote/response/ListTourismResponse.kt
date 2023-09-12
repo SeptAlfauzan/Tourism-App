9f8d499644957d065afe6bac1c809d468e74bcd2
SeptAlfauzan <alfauzansepta@gmail.com>
2023-09-12 11:40:57 +0700

    remove unnecessary files

--- app/src/main/java/com/dicoding/tourismapp/core/data/source/remote/response/ListTourismResponse.kt
+++
@@ -1,3 +0,0 @@
-package com.dicoding.tourismapp.core.data.source.remote.response
-
-data class ListTourismResponse()
--- app/src/main/java/com/dicoding/tourismapp/core/domain/model/Tourism.kt
+++
@@ -1,3 +0,0 @@
-package com.dicoding.tourismapp.core.domain.model
-
-data class Tourism()
